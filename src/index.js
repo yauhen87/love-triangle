0de2a1c03725ec414594fb4a5e54e4f8224ece33
Yauhen Vasilyeu <yauhen.vasilyeu@gmail.com>
2019-03-19 21:50:38 +0300

    refactor: a code change that simplify algoritm in index.js

--- src/index.js
+++ src/index.js
@@ -5,17 +5,13 @@
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
   let count = 0;
+  let b = [];
   for (let i = 0; i < preferences.length; i += 1) {    
     let n = preferences[i];
     let k = preferences[n-1];
     let l = preferences[k-1];
     if (l - 1 == i) {
-      const counterN = n;
-      const counterK = k;
-      const counterL = l;
-      if (counterN != counterK || counterN != counterL) {
-        count += 1;
-      }      
+        count += 1;   
     }
   }
   return count;
