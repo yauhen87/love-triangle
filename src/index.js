1c4cae1ffa4b5d8aba038d4077c5d847012265b7
Yauhen Vasilyeu <yauhen.vasilyeu@gmail.com>
2019-03-19 19:56:19 +0300

    feat: add first code in index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+  for (let i=0; i < preferences.length; i++) {    
+    let n = preferences[i];
+    let k = preferences[n];
+    let l = preferences[k];
+    if (l == i) {
+      count += 1;
+    }
+  }
+  return count;
 };
